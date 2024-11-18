a9ffcdcc92188e6591c696036bf148a69f890d00
sophieshixue <xueshi@ucsb.edu>
2024-11-17 18:22:57 -0800

    Update sophie.R

--- scripts/sophie.R
+++ scripts/sophie.R
@@ -78,3 +78,34 @@ accuracy <- mean(predicted_classes == pca_test$bclass)
 roc_curve <- roc(word_test$bclass, predictions)
 auc(roc_curve)
 
+## bigrams
+set.seed(123)
+split_bigram <- initial_split(text_bigram, 
+                       prop = 0.8,
+                       strata = 'bclass')
+
+bigram_train <- training(split_bigram)
+bigram_test <- testing(split_bigram)
+bigram_y_train <- as.numeric(bigram_train$bclass)
+bigram_x_train <- bigram_train %>% select(-.id,-bclass, -mclass)
+bigram_y_test <- as.numeric(bigram_test$bclass)
+bigram_x_test <- bigram_test %>% select(-.id,-bclass, -mclass)
+
+lr1 <- glmnet(bigram_x_train, bigram_y_train, family = "binomial", alpha = 1)
+log_odds_train <- predict(lr1, newx = as.matrix(bigram_x_train), type = "link")
+log_odds_test <- predict(lr1, newx = as.matrix(bigram_x_test), type = "link")
+
+#PCA
+library(irlba)
+pca_bigram_train <- prcomp_irlba(bigram_x_train, n = 10)
+
+X_train_pca <- predict(pca_bigram_train, newdata = bigram_x_train)  # exceed limit
+X_test_pca <- predict(pca_bigram_train, newdata = bigram_x_test)  
+
+train_combined <- cbind(log_odds_train, X_train_pca)
+test_combined <- cbind(log_odds_test, X_test_pca)
+
+lr2 <- glm(bclass ~ ., data = data.frame(train_combined, labels = bigram_train$bclass), family = "binomial")
+y_pred <- predict(lr2, newdata = data.frame(test_combined), type = "response")
+
+y_pred_class <- ifelse(y_pred > 0.5, 1, 0)
